5dc51ff2447244526fa40c9480f6a87b7d5eba05
Atsushi-Takayama <149247777+Atsushi-Takayama@users.noreply.github.com>
2023-11-18 23:57:59 +0900

    Add files via upload

---
+++ kadai_010/summary.js
@@ -0,0 +1,14 @@
+$(function(){
+  $("#change-color").on("click",function(){
+    $("#target").css("color","red");
+  });
+  $("#change-text").on("click",function(){
+    $("#target").text("Hello!");
+  });
+  $("#fadeOut").on("click",function(){
+    $("#target").fadeOut();
+  });
+  $("#fadeIn").on("click",function(){
+    $("#target").fadeIn();
+  });
+});
